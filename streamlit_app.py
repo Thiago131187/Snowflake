cbc1afa9bcc7081c06d83f2673706d88f4604c81
Thiago131187 <110860778+Thiago131187@users.noreply.github.com>
2022-08-08 20:32:13 -0300

    Busca pelo nome da fruta
    Busca pelo nome da fruta

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,8 @@ streamlit.text('🥑🍞 hard=Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Crie seu próprio smoothie de frutas 🥝🍇')
 import pandas 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+my_fruit_list = streamlit.set_index('Fruit')
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+streamlit.dataframe(my_fruit_list)
