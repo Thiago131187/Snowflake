d2d6a24cea3f4476379b51abe24f54fc3951c2a3
Thiago131187 <110860778+Thiago131187@users.noreply.github.com>
2022-08-08 20:12:34 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Driver')
 
+streamlit.header('My Parents New Healthy Driver')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('hard=Boiled Free-Range Egg')
