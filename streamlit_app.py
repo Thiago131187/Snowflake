9015537f74231afcbd3b364658bf220669498816
Thiago131187 <110860778+Thiago131187@users.noreply.github.com>
2022-08-08 20:24:51 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,6 @@ streamlit.text('🥑🍞 hard=Boiled Free-Range Egg')
 
 
 streamlit.header('🍌🥭 Crie seu próprio smoothie de frutas 🥝🍇')
-
+import pandas 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
